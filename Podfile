ccde6a0224b42ac3e7846b3dfccf9573fa9cfa1a
Michal Mizera <1938540+Axadiw@users.noreply.github.com>
2020-03-29 22:14:32 +0200

    Init commit

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '12.0'
+inhibit_all_warnings!
+use_frameworks!
+
+target 'cocoapods-configuration-issue' do
+  pod 'BugfenderSDK', '1.8', :configurations => ['Debug_dev', 'Release_dev']
+  pod 'Firebase/Crashlytics', '6.21.0', :configurations => ['Debug_dev', 'Release_dev']
+end
---
+++ cocoapods-configuration-issue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cocoapods-configuration-issue
+//
+//  Created by Michal Mizera on 29/03/2020.
+//  Copyright © 2020 Axadiw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
